86947290d3a53629f60e2cdb4171e67120cba248
Sakib-Zaman-250 <sakibzaman250@gmail.com>
2023-10-14 13:40:38 +0600

    random password generator

--- RandomPassword/script.js
+++ RandomPassword/script.js
@@ -1,5 +1,5 @@
 const passwordBox = document.getElementById("password");
-const length = 8;
+const length = 12;
 
 const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase ="abcdefghijklmnopqrstuvwxyz";
